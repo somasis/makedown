8db0d646c00a5b386c655c483a301ddc224c134e
Kylie McClain <kylie@somasis.com>
2017-11-27 01:52:31 -0500

    update-submodule: show a diff and make a more informative commit msg

--- update-submodule.sh
+++ update-submodule.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 set -xe
+before=$(git -C makedown rev-parse --short HEAD)
 git submodule update --checkout --remote --force makedown
+PAGER=cat git diff --submodule makedown
+after=$(git -C makedown rev-parse --short HEAD)
 git add -v makedown
-git commit -o makedown -m 'Update makedown'
+git commit -o makedown -m "Update makedown (${before}..${after})"
 
